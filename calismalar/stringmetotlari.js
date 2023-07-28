f1970668ae84da1cc3fab15a538e15041991204d
Recepurkun <recep.urkun@hotmail.com>
2023-07-28 18:49:10 +0300

    String metot notlari eklendi

---
+++ calismalar/stringmetotlari.js
@@ -0,0 +1,82 @@
+// * notlar enes bayram adli youtube kanalina aittir.
+// ! use "better Comments" for better documentation
+
+// String Sınıfının Metotları
+
+let kurs ="Modern Web Geliştirme Kursu";
+let tarih = "2022";
+
+/*
+ charAt()
+ concat()
+ indexof()
+ lastindexof()
+ toUpperCase()
+ toLowerCase()
+ trim()
+ slice()
+ substring()
+ replace()
+ split()
+ valueOf()
+ startsWith()
+ endsWith();
+ */
+
+// * charAt Metot Kullanimi
+// let karakter = kurs.charAt(1);
+// console.log(karakter);
+
+// * concat Metot Kullanimi
+// let sonuc = kurs.concat(" ",tarih," Enes");
+// console.log(sonuc);
+
+// * indexOf Metot Kullanimi
+// let index = kurs.indexOf("W");
+// console.log(index);
+
+// let index = kurs.lastIndexOf("K");
+// console.log(index);
+
+// *  toUpperCase Metot Kullanimi
+// let sonuc = kurs.toUpperCase();
+// console.log(kurs);
+// console.log(sonuc);
+
+// *  toLowerCase Metot Kullanimi
+// let sonuc = kurs.toLowerCase();
+// console.log(sonuc);
+
+// *  Trim Metot Kullanimi
+// console.log(kurs.trim());
+
+// *  Slice Metot Kullanimi
+// console.log(kurs);
+// console.log(kurs.slice(22,28));
+
+// *  substring Metot Kullanimi
+// console.log(kurs);
+// console.log(kurs.substring(0,6));
+
+// *  Replace Metot Kullanimi
+// console.log(kurs);
+// console.log(kurs.replace("Modern","Güncel"));
+
+// *  Split Metot Kullanimi
+// console.log(kurs);
+// let dizi = kurs.split(",");
+// console.log(dizi);
+
+// *  valueOf Metot Kullanimi
+// console.log(kurs.valueOf());
+
+// *  startsWith Metot Kullanimi
+// console.log(kurs);
+// console.log(kurs.startsWith("M"));
+// if(kurs.startsWith("M")){
+//     console.log("M harfi ile başlamaktadır.");
+// }
+
+// * endsWith Metot Kullanimi
+// console.log(kurs);
+// console.log(kurs.endsWith("Geliştirme Kursu"));
